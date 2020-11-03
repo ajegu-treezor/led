f7c8b08588414876549313a48609d7efa936216e
Allan JEGU <allan.jegu@treezor.com>
2020-11-03 17:31:40 +0100

    Led get method

--- app/Http/Controllers/LedController.php
+++ app/Http/Controllers/LedController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Commands\LedGetCommand;
 use App\Response\IlluminateJsonResponseAdapter;
 use App\Commands\LedAllCommand;
 use App\Repositories\LedRepositoryInterface;
@@ -13,6 +14,11 @@ use Laravel\Lumen\Routing\Controller;
 
 class LedController extends Controller
 {
+    /**
+     * @param LedRepositoryInterface $ledRepo
+     * @param LedDTOMapper $ledMapper
+     * @return JsonResponse
+     */
     public function getAll(LedRepositoryInterface $ledRepo, LedDTOMapper $ledMapper): JsonResponse
     {
         $responseAdapter = new IlluminateJsonResponseAdapter();
@@ -21,4 +27,20 @@ class LedController extends Controller
 
         return $responseAdapter->getResponse();
     }
+
+    /**
+     * @param string $ledId
+     * @param LedRepositoryInterface $ledRepo
+     * @param LedDTOMapper $ledMapper
+     * @return JsonResponse
+     */
+    public function get(string $ledId, LedRepositoryInterface $ledRepo, LedDTOMapper $ledMapper): JsonResponse
+    {
+        $responseAdapter = new IlluminateJsonResponseAdapter();
+        $command = new LedGetCommand($ledId, $ledRepo, $ledMapper, $responseAdapter);
+        $command->execute();
+
+        return $responseAdapter->getResponse();
+    }
+
 }
